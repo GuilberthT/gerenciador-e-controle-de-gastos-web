6adefa9d22e4bb3b3939d42f32644d144e985b2a
Matheus Rocha Sandim <mrochasandim75@gmail.com>
2024-05-11 16:21:02 -0300

    refactor: compostion api

--- src/components/ExpenseList.vue
+++ src/components/ExpenseList.vue
@@ -1,34 +1,24 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import ExpenseService from '@/api/ExpenseService.js';
+
+const expenses = ref([]);
+
+onMounted(async () => {
+  expenses.value = await ExpenseService.getExpenses();
+});
+</script>
+
+
 <template>
-    <div class="container">
-      <h2>Lista de Despesas</h2>
-      <ul>
-        <li v-for="expense in expenses" :key="expense._id">
-          {{ expense.description }} - R$ {{ expense.amount }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  import ExpenseService from '@/api/ExpenseService.js';
-  
-  export default {
-    setup() {
-      const expenses = ref([]);
-  
-      onMounted(async () => {
-        expenses.value = await ExpenseService.getExpenses();
-      });
-  
-      return {
-        expenses
-      };
-    }
-  };
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  <div class="container">
+    <h2>Lista de Despesas</h2>
+    <ul>
+      <li v-for="expense in expenses" :key="expense._id">
+        {{ expense.description }} - R$ {{ expense.amount }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
